283a763920ea0b191b2fd7daefb79a43b4705681
Sarthakkk <166196646+jndlsarthak@users.noreply.github.com>
2026-08-22 12:13:32 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # s3 =  [s2[i:j] for i in range(len(s2)) for j in range(i + 1, len(s2) + 1)]
+
+        # perms = [""]
+        # for char in s1:
+        #     new_perms = []
+        #     for perm in perms:
+        #         for i in range(len(perm) + 1):
+        #             new_perms.append(perm[:i] + char + perm[i:])
+        #     perms = new_perms
+
+
+
+        # for k in perms:
+        #     if k in s3:
+        #         return True
+        #     else:
+        #         return False 
+
+
+
+        count1 = {}
+        for c in s1:
+            count1[c] = 1+ count1.get(c,0)
+        need = len(count1)
+        for i in range(len(s2)):
+            count2, cur = {}, 0
+            for j in range(i,len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0)
+                if count1.get(s2[j], 0) < count2.get(s2[j]):
+                    break
+                if count1.get(s2[j], 0) == count2.get(s2[j]):
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
+
+
+        
